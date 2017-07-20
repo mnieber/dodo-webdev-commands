58b38ffb397aee32c68a741edd1de77ec9ab146e
Maarten Nieber <hallomaarten@yahoo.com>
2017-07-20 23:48:33 +0200

    Use dockerupdate in combination with the docker decorator

--- dodo_webdev_commands/dockerupdate.py
+++ dodo_webdev_commands/dockerupdate.py
@@ -1,25 +1,23 @@
 # noqa
-from dodo_commands.extra.standard_commands import DodoCommand, CommandError
+from dodo_commands.extra.standard_commands import DodoCommand
 from plumbum.cmd import docker
 
 
 class Command(DodoCommand):  # noqa
     help = ""
+    safe = False
+
+    docker_rm = False
+    docker_options = [
+        ('name', 'dockerupdate')
+    ]
 
     def add_arguments_imp(self, parser):  # noqa
         parser.add_argument('--playbook')
-        parser.add_argument('--input-image')
         parser.add_argument('--tags')
 
     def _playbook(self, playbook):
         return playbook or self.get_config("/ANSIBLE/default_playbook")
-
-    def _input_image_name(self, input_image):
-        return input_image or self.get_config("/DOCKER/image")
-
-    @property
-    def _output_image_name(self):
-        return self.get_config("/DOCKER/image")
 
     @property
     def _ansible_dir(self):
@@ -29,33 +27,25 @@ class Command(DodoCommand):  # noqa
     def _remote_ansible_dir(self):
         return "/root/ansible/"
 
-    def _check_existing_image(self, input_image):
-        input_image_name = self._input_image_name(input_image)
-        result = docker("images", "-a", "--quiet", input_image_name)
-        if not result:
-            raise CommandError("Cannot find image %s" % input_image_name)
-
     def _tags(self, tags):
         return "--tags=%s" % tags if tags else ""
 
-    def handle_imp(self, playbook, input_image, tags, **kwargs):  # noqa
-        self._check_existing_image(input_image)
+    def handle_imp(self, playbook, tags, **kwargs):  # noqa
+        self.docker_options.append(
+            ('volume', '%s:%s' % (self._ansible_dir, self._remote_ansible_dir))
+        )
+
         self.runcmd(
             [
-                "docker",
-                "run",
-                "-w", self._remote_ansible_dir,
-                "--volume=%s:%s" % (self._ansible_dir, self._remote_ansible_dir),
-                self._input_image_name(input_image),
-                "/bin/bash",
-                "-c",
-                "ansible-playbook -i hosts -l localhost %s %s" % (
-                    self._playbook(playbook),
-                    self._tags(tags)
-                )
-            ]
+                "ansible-playbook",
+                "-i", "hosts",
+                "-l", "localhost",
+                self._playbook(playbook),
+                self._tags(tags)
+            ],
+            cwd=self._remote_ansible_dir
         )
 
         container_id = docker("ps", "-l", "-q")[:-1]
-        self.runcmd(["docker", "commit", container_id, self._output_image_name])
-        self.runcmd(["docker", "rm", container_id])
+        docker("commit", container_id, self.get_config("/DOCKER/image"))
+        docker("rm", container_id)

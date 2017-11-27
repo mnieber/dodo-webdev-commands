335512988b79d6f9bac15d11cf43a5a3d3f7151b
Maarten Nieber <hallomaarten@yahoo.com>
2017-11-27 12:24:51 +0100

    Use value /DJANGO/port in django-runserver

--- dodo_webdev_commands/django-runserver.py
+++ dodo_webdev_commands/django-runserver.py
@@ -5,9 +5,10 @@ from dodo_commands.framework.util import remove_trailing_dashes
 
 
 class Command(DodoCommand):  # noqa
+    default_port = 8000
+
     docker_options = [
         ('name', 'django-runserver'),
-        ('publish', '8000:8000'),
     ]
 
     def add_arguments_imp(self, parser):  # noqa
@@ -17,12 +18,16 @@ class Command(DodoCommand):  # noqa
         )
 
     def handle_imp(self, runserver_args, *args, **kwargs):  # noqa
+        port = self.get_config("/DJANGO/port", "8000")
+        Command.docker_options.append(
+            ('publish', "%s:%s" % (port, port))
+        )
 
         self.runcmd(
             [
                 self.get_config("/DJANGO/python"),
                 "manage.py",
-                "runserver", "0.0.0.0:8000",
+                "runserver", "0.0.0.0:%s" % port,
             ]
             + remove_trailing_dashes(runserver_args)
             + self.get_config("/DJANGO/runserver_args", []),

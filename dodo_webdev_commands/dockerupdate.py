2d649cbffa5de8cc3727355b463017d5ca30765c
Maarten Nieber <hallomaarten@yahoo.com>
2018-01-19 12:56:35 +0100

    Make dockerupdate a generic command

--- dodo_webdev_commands/dockerupdate.py
+++ dodo_webdev_commands/dockerupdate.py
@@ -12,38 +12,10 @@ class Command(DodoCommand):  # noqa
         ('name', 'dockerupdate')
     ]
 
-    def add_arguments_imp(self, parser):  # noqa
-        parser.add_argument('--playbook')
-        parser.add_argument('--tags')
-
-    def _playbook(self, playbook):
-        return playbook or self.get_config("/ANSIBLE/default_playbook")
-
-    @property
-    def _ansible_dir(self):
-        return self.get_config("/ANSIBLE/src_dir")
-
-    @property
-    def _remote_ansible_dir(self):
-        return "/root/ansible/"
-
-    def _tags(self, tags):
-        return ["--tags=%s" % tags] if tags else []
-
-    def handle_imp(self, playbook, tags, **kwargs):  # noqa
-        self.docker_options.append(
-            ('volume', '%s:%s' % (self._ansible_dir, self._remote_ansible_dir))
-        )
-
+    def handle_imp(self, **kwargs):  # noqa
         self.runcmd(
-            [
-                "ansible-playbook",
-                "-i", "hosts",
-                "-l", "localhost",
-                self._playbook(playbook)
-            ] +
-            self._tags(tags),
-            cwd=self._remote_ansible_dir
+            self.get_config("/DOCKER/update_args"),
+            cwd=self.get_config("/DOCKER/update_cwd")
         )
 
         container_id = docker("ps", "-l", "-q")[:-1]

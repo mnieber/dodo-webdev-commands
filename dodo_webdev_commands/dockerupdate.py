17e71f317e62450ec7308a71ac7b1f33d03b63f5
Maarten Nieber <hallomaarten@yahoo.com>
2018-02-26 17:13:42 +0100

    Make dockerupdate support salt, instead of being generic

--- dodo_webdev_commands/dockerupdate.py
+++ dodo_webdev_commands/dockerupdate.py
@@ -13,10 +13,29 @@ class Command(DodoCommand):  # noqa
     ]
 
     def handle_imp(self, **kwargs):  # noqa
-        self.runcmd(
-            self.get_config("/DOCKER/update_args"),
-            cwd=self.get_config("/DOCKER/update_cwd")
-        )
+        salt_cwd = self.get_config('/DOCKER/salt/cwd', '')
+        if salt_cwd:
+            pillar_root = self.get_config(
+                'DOCKER/salt/pillar_root', './pillar'
+            )
+            file_root = self.get_config(
+                'DOCKER/salt/file_root', '.'
+            )
+            args = (
+                [
+                    'salt-call',
+                    '--local',
+                    '--file-root=%s' % file_root,
+                    '--pillar-root=%s' % pillar_root,
+                ]
+                + self.get_config('DOCKER/salt/extra_args', [])
+                + [
+                    'state.apply'
+                ]
+            )
+            self.runcmd(args, cwd=salt_cwd)
+        else:
+            return
 
         container_id = docker("ps", "-l", "-q")[:-1]
         docker("commit", container_id, self.get_config("/DOCKER/image"))

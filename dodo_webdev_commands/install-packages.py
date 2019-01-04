b497a886e910c2177183514b8cd62bda712ce53a
Maarten Nieber <hallomaarten@yahoo.com>
2019-01-04 17:12:09 +0100

    Refactor install-packages

--- dodo_webdev_commands/install-packages.py
+++ dodo_webdev_commands/install-packages.py
@@ -7,15 +7,17 @@ def _args():
     parser = ArgumentParser()
     args = Dodo.parse_args(parser)
     args.node_modules_dir = Dodo.get_config('/SERVER/node_modules_dir')
+    args.pip = os.path.join(Dodo.get_config('/SERVER/venv_dir'), 'bin', 'pip')
+    args.yarn = 'yarn'
     return args
 
 
 if Dodo.is_main(__name__, safe=True):
     args = _args()
 
-    pip = os.path.join(Dodo.get_config('/SERVER/venv_dir'), 'bin', 'pip')
     requirements_filename = Dodo.get_config('/SERVER/pip_requirements')
 
-    Dodo.run([pip, 'install', '-r', requirements_filename])
-    Dodo.run(['yarn', 'install'],
-             cwd=os.path.abspath(os.path.join(args.node_modules_dir, '..')))
+    Dodo.run([args.pip, 'install', '-r', requirements_filename])
+    Dodo.run(
+        [args.yarn, 'install'],
+        cwd=os.path.abspath(os.path.join(args.node_modules_dir, '..')))

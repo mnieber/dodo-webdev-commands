aecbe49960a9cfb7ff97569fa1b3e1fd52e0e5dd
Maarten Nieber <hallomaarten@yahoo.com>
2019-01-26 21:40:30 +0100

    Add --name argument to yarn

--- dodo_webdev_commands/yarn.py
+++ dodo_webdev_commands/yarn.py
@@ -6,6 +6,7 @@ from dodo_commands.framework.util import remove_trailing_dashes
 def _args():
     parser = argparse.ArgumentParser()
     parser.add_argument('yarn_args', nargs=argparse.REMAINDER)
+    parser.add_argument('--name')
     args = Dodo.parse_args(parser)
     args.yarn = 'yarn'
     args.cwd = Dodo.get_config('/WEBPACK/webpack_dir')
@@ -14,5 +15,11 @@ def _args():
 
 if Dodo.is_main(__name__, safe=True):
     args = _args()
+
+    if args.name:
+        Dodo.config['DOCKER'] \
+            .setdefault('options', {}) \
+            .setdefault('yarn', {})['name'] = args.name
+
     Dodo.run(
         [args.yarn] + remove_trailing_dashes(args.yarn_args), cwd=args.cwd)

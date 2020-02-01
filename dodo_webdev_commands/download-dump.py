786f931f26c044671a19e6dc5925c846538cd293
Maarten Nieber <hallomaarten@yahoo.com>
2020-02-01 15:48:11 +0100

    Use ChoicePicker

--- dodo_webdev_commands/download-dump.py
+++ dodo_webdev_commands/download-dump.py
@@ -1,9 +1,9 @@
 import os
 from argparse import ArgumentParser
 
-from dodo_commands import Dodo, CommandError
+from dodo_commands import Dodo
 from dodo_commands.framework.config import merge_into_config
-from dodo_commands.framework.util import filter_choices
+from dodo_commands.framework.choice_picker import ChoicePicker
 from dodo_ssh_agent_commands.utils import add_ssh_agent_args
 
 
@@ -27,29 +27,28 @@ if Dodo.is_main(__name__, safe=True):
 
     merge_into_config(
         Dodo.get_config('/DOCKER').setdefault('options', {}),
-        {Dodo.command_name: add_ssh_agent_args({
-            'is_interactive': False
-        })})
+        {Dodo.command_name: add_ssh_agent_args({'is_interactive': False})})
 
-    dump_fns = Dodo.run(
-        [
-            'ssh',
-            'root@%s' % args.host,
-            'ls',
-            os.path.join(dumps_dir, '*.sql'),
-        ],
-        capture=True).split()
+    dump_fns = Dodo.run([
+        'ssh',
+        'root@%s' % args.host,
+        'ls',
+        os.path.join(dumps_dir, '*.sql'),
+    ],
+                        capture=True).split()
 
     dump_fns = sorted([x for x in dump_fns if x], key=_get_timestamp)
 
-    for idx, dump_fn in enumerate(dump_fns):
-        print("%d: %s" % (idx + 1, dump_fn))
+    class Picker(ChoicePicker):
+        def print_choices(self, choices):
+            for idx, dump_fn in enumerate(choices):
+                print("%d: %s" % (idx + 1, dump_fn))
 
-    raw_choice = raw_input('Select files to download (e.g. 1,3-4): ')
-    try:
-        selected_fns, span = filter_choices(dump_fns, raw_choice)
-    except Exception as e:
-        raise CommandError(str(e))
+        def question(self):
+            return 'Select files to download (e.g. 1,3-4): '
 
-    for dump_fn in selected_fns:
+    picker = Picker()
+    picker.pick(dump_fns)
+
+    for dump_fn in picker.get_choices():
         Dodo.run(['scp', 'root@%s:%s' % (args.host, dump_fn), dump_fn])

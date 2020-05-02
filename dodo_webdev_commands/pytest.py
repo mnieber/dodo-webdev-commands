76970ccc6d973b65155497a310443594ac40a115
Maarten Nieber <hallomaarten@yahoo.com>
2020-05-02 08:36:33 +0200

    WIP: remove --cov from pytest

--- dodo_webdev_commands/pytest.py
+++ dodo_webdev_commands/pytest.py
@@ -2,7 +2,6 @@ from dodo_commands import Dodo
 
 
 def _args():
-    Dodo.parser.add_argument('--cov', action='store_true')
     Dodo.parser.add_argument('pytest_args', nargs="*")
     args = Dodo.parse_args()
     args.no_capture = not Dodo.get_config("/PYTEST/capture", True)
@@ -12,17 +11,8 @@ def _args():
     args.pytest_ini_filename = Dodo.get_config("/PYTEST/pytest_ini", None)
     args.maxfail = Dodo.get_config("/PYTEST/maxfail", None)
     args.pytest = Dodo.get_config("/PYTEST/pytest", "pytest")
-    args.coverage_dirs = Dodo.get_config("/PYTEST/coverage_dirs", [])
     args.cwd = Dodo.get_config("/PYTEST/cwd", Dodo.get_config("/ROOT/src_dir"))
     return args
-
-
-def _cov_args(args):
-    cov_args = ["--cov=%s" % x for x in args.coverage_dirs]
-    cov_args.extend(['--cov-report', 'html:/opt/pincamp/website/cov_html'])
-    cov_args.extend(
-        ['--cov-report', 'annotate:/opt/pincamp/website/cov_annotate'])
-    return cov_args if args.cov else ['--no-cov'] if args.coverage_dirs else []
 
 
 if Dodo.is_main(__name__):
@@ -31,7 +21,6 @@ if Dodo.is_main(__name__):
     run_args = [
         *(args.pytest if isinstance(args.pytest, list) else [args.pytest]),
         *([args.test_file] if args.test_file else []),
-        *_cov_args(args),
         *(["--capture", "no"] if args.no_capture else []),
         *(["--reuse-db"] if args.reuse_db else []),
         *(["-c", args.pytest_ini_filename]

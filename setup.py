a1bf479132db79a6eed87fffc22352ebacd625b1
Maarten Nieber <hallomaarten@yahoo.com>
2024-02-15 12:32:20 +0100

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='dodo-webdev-commands',
-      version='0.20.0',
+      version='0.21.0',
       description='Web development related Dodo Commands',
       url='https://github.com/mnieber/dodo-webdev-commands',
       download_url=
-      'https://github.com/mnieber/dodo-webdev-commands/tarball/0.20.0',
+      'https://github.com/mnieber/dodo-webdev-commands/tarball/0.21.0',
       author='Maarten Nieber',
       author_email='hallomaarten@yahoo.com',
       license='MIT',
